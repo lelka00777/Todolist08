a76d481d6a5cf67cc846ad887e25dccab7f03dc4
lelka00777 <lelka00777@gmail.com>
2023-04-09 13:39:20 +0300

    Updates

---
+++ precache-manifest.1ad333d76dc2401ae3b0f709d47ad72d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d476efce41d0ecd2d6606991b104249d",
+    "url": "/Todolist08/index.html"
+  },
+  {
+    "revision": "add4438a7b22fdb58670",
+    "url": "/Todolist08/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "4501343d239dd96e78bd",
+    "url": "/Todolist08/static/js/2.dad71184.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todolist08/static/js/2.dad71184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add4438a7b22fdb58670",
+    "url": "/Todolist08/static/js/main.1e862904.chunk.js"
+  },
+  {
+    "revision": "6875b9a4dd7fd061c602",
+    "url": "/Todolist08/static/js/runtime-main.5b479e9f.js"
+  }
+]);
